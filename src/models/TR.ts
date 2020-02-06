b3372735e4f8fe941c21e31563aba98fe0f54a69
Ronald <ronald.scruz16@gmail.com>
2020-02-06 09:13:33 -0300

    Reading sheet and his values

--- src/models/TR.ts
+++ src/models/TR.ts
@@ -1,6 +1,5 @@
 import xlsx from 'xlsx';
 import { saveFileToDownloads } from '../utils/files';
-
 class TR {
   ALL_TR_URL: string;
   DOWNLOADS_PATH: string;
@@ -8,15 +7,17 @@ class TR {
   constructor() {
     this.ALL_TR_URL =
       'https://ww2.trtsp.jus.br/fileadmin/tabelas-praticas/gerador_indices_tr.xls';
-    this.DOWNLOADS_PATH = `${__dirname}/../downloads/`;
+    this.DOWNLOADS_PATH = `${__dirname}/../downloads`;
   }
 
   async all() {
     const savedFileName = 'TR_All.xlsx';
-
     await saveFileToDownloads(this.ALL_TR_URL, savedFileName);
 
-    console.log(xlsx.readFile(this.DOWNLOADS_PATH + savedFileName));
+    const trIndexesSheet = xlsx.readFile(`${this.DOWNLOADS_PATH}/${savedFileName}`)
+      .Sheets['Índices diários'];
+
+    console.log(Object.keys(trIndexesSheet));
   }
 }
 

0c1d4fd4d79979431aaac38a0a4322ff3b9f2051
Ronald <ronald.scruz16@gmail.com>
2020-02-07 09:21:39 -0300

    Started TR routing

--- src/controllers/TR.ts
+++ src/controllers/TR.ts
@@ -2,33 +2,33 @@ import TR from '../models/TR';
 import { Index } from '../types/Index';
 
 class TRController {
-  async all(): Promise<Index[] | boolean> {
+  async all(): Promise<Index[]> {
     try {
       const allTr = await TR.all();
       return allTr;
     } catch (err) {
       console.error('Failed fetching all TR indexes.', err);
-      return false;
+      return err;
     }
   }
 
-  async one(date?: string): Promise<Index | boolean> {
+  async one(date?: string): Promise<Index> {
     try {
       const tr = await TR.one(date);
       return tr;
     } catch (err) {
       console.error('Failed fetching requested TR index.', err);
-      return false;
+      return err;
     }
   }
 
-  async last(): Promise<Index | boolean> {
+  async last(): Promise<Index> {
     try {
       const lastTr = await TR.last();
       return lastTr;
     } catch (err) {
       console.error('Failed fetching last TR index.', err);
-      return false;
+      return err;
     }
   }
 }

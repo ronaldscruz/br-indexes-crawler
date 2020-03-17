30cbd08e11a3892f6f82076186ccff240034e04b
Ronald <ronald.scruz16@gmail.com>
2020-03-17 16:09:33 -0300

    Added routines folder

---
+++ routines/README.md
@@ -0,0 +1,3 @@
+# WIP - Routines
+
+Routines are responsible for inserting API indexes in database. It is seriously recommended to run them inside an Apache folder (/var/www/html in Ubuntu). They're completely customizable, since they're made to supply different databases and needs. Also, logs dont work properly xD
---
+++ routines/config.php
@@ -0,0 +1,15 @@
+<?php
+  // Php
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
+
+  // Database
+  define("DB_HOST", "localhost");
+  define("DB_NAME", "indexes_crawler");
+  define("DB_USER", "root");
+  define("DB_PASS", "Mawchine@18");
+
+  // API
+  define("API_URL", "http://localhost:4500");
+?>
